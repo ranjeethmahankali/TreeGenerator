d95e2357d75832209a1ef5e6685cfc7f9324b707
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2016-06-28 09:45:49 +0530

    finshed writing the depestChild method in the new version of the program

--- randTree.py
+++ randTree.py
@@ -29,11 +29,42 @@ class node:
 		else:
 			return 1+self.parent.depth()
 			
-	def renderSubTree(self):
+	def renderSubTreeWF(self):
 		#render the subtree starting at this node
+		#but renders just a wireframe
 		for ch in self.child:
 			rs.AddLine(self.pos, ch.pos)
-			ch.renderSubTree()
+			ch.renderSubTreeWF()
+	
+	def deepestChild(self, depth = 0):			
+		if len(self.child)  ==  0:
+			return [self,depth]
+		else:
+			maxDepth = depth
+			deepChild = self
+			for ch in self.child:
+				childDepth = ch.deepestChild(1+depth)
+				if maxDepth < childDepth[1]:
+					maxDepth = childDepth[1]
+					deepChild = childDepth[0]
+				
+			return [deepChild,maxDepth]
+			
+	def UnfDeepestChild(self, depth = 0):			
+		if len(self.child)  ==  0:
+			return [self,depth]
+		else:
+			maxDepth = depth
+			deepChild = self
+			for ch in self.child:
+				if ch.isDone == True:
+					continue
+				childDepth = ch.deepestChild(1+depth)
+				if maxDepth < childDepth[1]:
+					maxDepth = childDepth[1]
+					deepChild = childDepth[0]
+				
+			return [deepChild,maxDepth]
 
 def randomPt(x1,x2,y1,y2,z1,z2):
 	px = random.uniform(x1,x2)
@@ -46,7 +77,7 @@ def grow(num):
 	if num == 0:
 		return 0
 	randPt = randomPt(-50,50,-50,50,0,100)
-	print(randomPt)
+	
 	joinNode = nodeList[rs.PointArrayClosestPoint(ptArray, randPt)]
 	joinPt = joinNode.pos
 	joinVec = rs.VectorSubtract(randPt, joinPt)
@@ -55,54 +86,16 @@ def grow(num):
 	newPt = rs.VectorAdd(joinPt, growthVec)
 	
 	newNode = node(newPt, joinNode)
-	grow(num-1)
-		
-def maxDepth():
-	maxD = 0
-	nodeNum = None
-	for nd in nodeList:
-		if len(nd.child) == 0 and not nd.isDone:
-			dpth = nd.depth()
-			if dpth > maxD:
-				maxD = dpth
-				nodeNum = nodeList.index(nd)
-	
-	return [nodeNum, maxD]
+	g = grow(num-1)
 
 rs.EnableRedraw(False)
 root = node([0,0,0])
 
-g = grow(100)
+g = grow(20)
 
-#root.renderSubTree()
-#this function is the one that has all the problems
-def renderTree():
-	duplicateNodes = nodeList[:]
-	while len(duplicateNodes) > 0:
-		ptList = []
-		maxD = maxDepth()
-		if maxD is None:
-			return 0
-		ptList.append(nodeList[maxD[0]].pos)
-		n = maxD[0]
-		while (not nodeList[n].parent is None):
-			if not nodeList[n].parent.isDone:
-				ptList.append(nodeList[n].parent.pos)
-				n = nodeList.index(nodeList[n].parent)
-			else:
-				break
-		
-		cur = rs.AddCurve(ptList,1)
-		#print(len(duplicateNodes))
-		duplicateNodes.remove(nodeList[maxD[0]])
-		nodeList[maxD[0]].isDone = True
-		
+root.renderSubTreeWF()
+deepCh = root.deepestChild()
+rs.AddPoint(deepCh[0].pos)
+print(deepCh)
 
-#root.renderSubTree()
-renderTree()
-rs.EnableRedraw(True)
-print('yeah')
-wait = input('waiting...')
-rs.EnableRedraw(False)
-root.renderSubTree()
 rs.EnableRedraw(True)
